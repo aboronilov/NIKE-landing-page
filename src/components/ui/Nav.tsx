e7f852c9413058a9404372aa072a6175d4eeb64f
Anatoly Boronilov <aboronilov@express42.com>
2023-09-05 12:04:12 +0300

    Navbar

---
+++ src/components/ui/Nav.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+import { headerLogo } from "@/assets/images"
+import { hamburger } from "@/assets/icons"
+import { navLinks } from '@/constants'
+
+type Props = {}
+
+const Nav = (props: Props) => {
+    return (
+        <header className='padding-x py-8 absolute z-10 w-full'>
+            <nav className='flex justify-between items-center max-container'>
+                <Link href="/">
+                    <div className="relative w-[130px] h-[29px]">
+                        <Image
+                            src={headerLogo}
+                            alt="logo"
+                        />
+                    </div>
+                </Link>
+                <ul className='flex flex-1 justify-center items-center gap-16 max-lg:hidden'>
+                    {navLinks.map((item) => (
+                        <Link 
+                            key={item.label} 
+                            href={item.href} 
+                            className='font-montserrat leading-normal text-lg text-slate-gray on-hover cursor-pointer'
+                        >
+                            {item.label}
+                        </Link>
+                    ))}
+                </ul>
+                <h1 className='font-montserrat leading-relaxed text-xl on-hover cursor-pointer'>Login</h1>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav
