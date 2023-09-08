38d99da139c6fa47a1165264dd4ae0463af694af
Anatoly Boronilov <aboronilov@express42.com>
2023-09-08 15:54:04 +0300

    services

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -1,10 +1,22 @@
+import { services } from '@/constants'
 import React from 'react'
+import { ServiceCard } from '@/components/ui'
+
 
 type Props = {}
 
 const Services = (props: Props) => {
   return (
-    <div>Services</div>
+    <section className='max-container flex justify-center flex-wrap gap-9'>
+    {services.map((item) => (
+      <ServiceCard 
+        key={item.label} 
+        label={item.label} 
+        imgURL={item.imgURL}
+        subtext={item.subtext}
+      />
+    ))}
+  </section>
   )
 }
 
